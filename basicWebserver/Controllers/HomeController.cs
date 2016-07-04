dc59280214edd2789fbb9ff1a34f416a8ccefea4
Hemstreet <jonhemstreet@gmail.com>
2016-07-04 02:12:42 -0600

    Updating basic route controller with optional params

--- basicWebserver/Controllers/HomeController.cs
+++ basicWebserver/Controllers/HomeController.cs
@@ -11,10 +11,19 @@ namespace aspnetcoreapp.Controllers
             return "Defualt Home controller root action";
         }
 
-        // GET: /Home/Test
-        public string Test(string name, int numTimes = 1)
+        // GET: /Home/Test/{id?}
+        public string Test(string name, int numTimes = 1, int id = 0)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, id: {numTimes}");
+
+            string html = $"Hello {name}, id: {numTimes}";
+
+            if(id > 0)
+            {
+                // We have passed in a custom ID value
+                html +=  $", optional ID: {id}";
+            }
+            
+            return HtmlEncoder.Default.Encode(html);
         }    
     }
 }
